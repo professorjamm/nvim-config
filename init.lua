98bfd17a3ea69ae1b3682895aeecf311d5de157f
professorjamm <faizaan.siddiqui015@gmail.com>
2024-04-06 03:33:42 -0500

    commiting basic nvim setup

---
+++ init.lua
@@ -0,0 +1,18 @@
+--lazy setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+local opts = {}
+
+--calling plugins and other settings 
+require("vim-options")
+require("lazy").setup("plugins")
